e58a2a232249485a95b53f5723e277149fd33be0
Dave Glad <63361139+DaveGlad@users.noreply.github.com>
2022-08-23 19:44:00 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navigation from "./Navigation";
+import Content from "./Content";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navigation />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
